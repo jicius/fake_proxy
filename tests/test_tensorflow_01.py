8876460a579282d05dc2be94afa46f6b128377b5
jicius <bq_ji@yahoo.com>
2017-09-10 10:29:46 +0800

    add: tf test

---
+++ tests/test_tensorflow_01.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+#   -*- coding: utf-8 -*-
+#
+#   Copyright (C) 2017 Jicius
+# 
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+# 
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+# 
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import tensorflow as tf
+import numpy as np
+
+# 使用 NumPy 生成假数据(phony data), 总共 100 个点.
+x_data = np.float32(np.random.rand(2, 100)) # 随机输入
+y_data = np.dot([0.100, 0.200], x_data) + 0.300
+
+# 构造一个线性模型
+#
+b = tf.Variable(tf.zeros([1]))
+W = tf.Variable(tf.random_uniform([1, 2], -1.0, 1.0))
+y = tf.matmul(W, x_data) + b
+
+# 最小化方差
+loss = tf.reduce_mean(tf.square(y - y_data))
+optimizer = tf.train.GradientDescentOptimizer(0.5)
+train = optimizer.minimize(loss)
+
+# 初始化变量
+init = tf.initialize_all_variables()
+
+# 启动图 (graph)
+sess = tf.Session()
+sess.run(init)
+
+# 拟合平面
+for step in xrange(0, 201):
+    sess.run(train)
+    if step % 20 == 0:
+        print step, sess.run(W), sess.run(b)
+
+# 得到最佳拟合结果 W: [[0.100  0.200]], b: [0.300]
